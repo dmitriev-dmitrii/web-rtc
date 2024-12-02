d6a9c9476f0a6872a72656d11419f49791a467b4
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2024-12-02 03:55:00 +0300

    work draft 3

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ wss.on('connection', (ws) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 server.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
